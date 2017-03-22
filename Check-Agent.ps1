10412d821442329743731c00027eec5b75eeed4a
Van Donley <vandonley@gmail.com>
2017-03-22 10:48:36 -0700

    Check-Agent Prototype
    First version of Check-Agent

---
+++ Check-Agent.ps1
@@ -0,0 +1,28 @@
+﻿Function Check-Agent
+{
+    $Agents = Get-Service | where {$_.Name -like "Advanced Monitoring Agent*"}
+    
+    foreach ($Agent in $Agents) {
+
+    if ($Agent.Status -ne "Running") {
+        $Agent | Start-Service
+        $Output = Write-Host "Starting $Agent.DisplayName service now"
+            "-----------------------------"
+            "Service is $Agent.Status"
+        return $Output
+    }
+    if ($Agent.Status -eq "Running"){
+        $Output = Write-Host "$Agent.DisplayName is running"
+        return $Output
+    }
+    else {
+         $Output = Write-Host "Troubleshoot $Agent.DisplayName"
+            "-----------------------------"
+            "Status:  $Agent.Status"
+            "Start Type:  $Agent.StartType"
+        return $Output
+        }
+    }
+}
+
+Check-Agent
