e3af38dc698ea5c250c7398899895119f9085e71
vandonley <vandonley@gmail.com>
2017-08-25 10:14:05 -0700

    Fix issues with older Win7 computers refusing to update Powershell execution policy

--- WinRM_for_Win7.bat
+++ WinRM_for_Win7.bat
@@ -1,3 +1,4 @@
 :: Enable WinRM on Win7 when check fails.
-
+powershell set-executionpolicy -executionpolicy remotesigned -force
 winrm quickconfig -q
+exit 0
--- pccEnable-WinRM.ps1
+++ pccEnable-WinRM.ps1
@@ -7,8 +7,37 @@ Last Updated:  Van Donely - 04/21/2017
 --------------------------
 #>
 
-function pccEnable-WinRM
-{
+# Better way to test remoting
+# http://www.leeholmes.com/blog/2009/11/20/testing-for-powershell-remoting-test-psremoting/
+
+function Test-PsRemoting 
+{ 
+    param( 
+        [Parameter(Mandatory=$false, Position=1)] 
+        [string]$computername = 'localhost'
+    ) 
+    
+    try 
+    { 
+        $errorActionPreference = "Stop" 
+        $result = Invoke-Command -ComputerName $computername { 1 } 
+    } 
+    catch 
+    { 
+        Write-Verbose $_ 
+        return $false 
+    } 
+    
+    ## I've never seen this happen, but if you want to be 
+    ## thorough.... 
+    if($result -ne 1) 
+    { 
+        Write-Verbose "Remoting to $computerName returned an unexpected result." 
+        return $false 
+    } 
+    
+    $true    
+}
 
 # Hashtable for return from function
 
@@ -16,21 +45,21 @@ function pccEnable-WinRM
 
 # Checks
 
-$WinRMService = Get-Service winrm
+$WinRMService = Test-PsRemoting
 $PSExePolicy = Get-ExecutionPolicy
 
 # Check if the WinRM service is running. If not, run Enable-PSRemoting.
 
     try { 
-        if ( $WinRMService.Status -ne 'Running' )
+        if ( $WinRMService -eq $false )
             {
-            $Return.RMServiceStatusBegin = $WinRMService.Status
+            $Return.RMServiceStatusBegin = (Get-Service winrm).Status
             $Return.EnableWinRMOut = Enable-PSRemoting -Force -SkipNetworkProfileCheck `
                 -ErrorAction Continue -WarningAction Continue
             $Return.RMServiceStatus = (Get-Service winrm).Status
             }
 
-            else { $Return.RMServiceStatus = $WinRMService.Status }
+            else { $Return.RMServiceStatus = (Get-Service winrm).Status }
         }
 
     catch [EXCEPTION] { $_.Exception | Format-List -Force }
@@ -38,10 +67,10 @@ $PSExePolicy = Get-ExecutionPolicy
 # Check to see if the Powershell execution policy is set to unrestricted and change if needed.
 
     try {
-        if ( $PSExePolicy -ne 'Unrestricted' )
+        if ( $PSExePolicy -ne 'RemoteSigned' )
             {
             $Return.PSExePolicyBegin = $PSExePolicy
-            $Return.PSExePolicyOut = Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force `
+            $Return.PSExePolicyOut = Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force `
                 -ErrorAction Continue -WarningAction Continue
             $Return.PSExePolicy = Get-ExecutionPolicy
             }
@@ -53,7 +82,7 @@ $PSExePolicy = Get-ExecutionPolicy
 
 # Return results from function. Exit with error if not in correct state.
 
-    if ( $Return.RMServiceStatus -ne 'Running' -or $Return.PSExePolicy -ne 'Unrestricted' )
+    if ( $Return.RMServiceStatus -ne 'Running' -or $Return.PSExePolicy -ne 'RemoteSigned' )
         {
         $Error.Clear()
         [string]$ErrorString = "Check Failure"
@@ -64,7 +93,3 @@ $PSExePolicy = Get-ExecutionPolicy
         }
         
 Return ( $Return | Format-List )
-
-}
-
-pccEnable-WinRM
