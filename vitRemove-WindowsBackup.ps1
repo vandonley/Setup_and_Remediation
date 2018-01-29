f5f9a97033f5ca2b77b6d3456999bd1cd793f887
vandonley <vandonley@gmail.com>
2018-01-29 12:01:11 -0800

    Full Script

--- vitRemove-WindowsBackup.ps1
+++ vitRemove-WindowsBackup.ps1
@@ -69,14 +69,73 @@ try {
     # Folders to look for
     $FileBackups = $Drive + ':\' + $Env:COMPUTERNAME
     $ImageBackups = $Drive + ':\WindowsImageBackup'
-    # Check to see if the folders exist, then delete
+    # Check to see if the files folder exist, then delete
     if (Test-Path -Path $FileBackups) {
-        $Return.Backup_Files = Remove-Item
-    }      
+        Remove-Item -Path $FileBackups -Recurse -Force
+        # If the folder no longer exists, report success
+        if (!(Test-Path -Path $FileBackups)) {
+            $Return.Backup_Files = "Successfully removed $FileBackups"
+        }
+        # If the files folder still exists, error
+        else {
+            $Return.Backup_Files = "Failed to remove $FileBackups"
+            $Return.Error_Count++
+        }
+    }
+    # If the files folder does not exist, report but don't error
+    else {
+        $Return.Backup_Files = "No backup files found at $FileBackups"
+    }
+    # Check to see if the image folder exist, then delete
+    if (Test-Path -Path $ImageBackups) {
+        Remove-Item -Path $ImageBackups -Recurse -Force
+        # If the image folder no longer exists, report success
+        if (!(Test-Path -Path $ImageBackups)) {
+            $Return.Backup_Images = "Successfully removed $ImageBackups"
+        }
+        else {
+            $Return.Backup_Images = "Failed to remove $ImageBackups"
+            $Return.Error_Count++
+        }
+    }
+    # If the image folder does not exist, report but don't error
+    else {
+        $Return.Backup_Images = "No backup files found at $ImageBackups"
+    }       
 }
 catch {
     $myException = $_.Exception | Format-List | Out-String
-    $Return.Drive_Change_Catch = $myException 
+    $Return.Backups_Removed_Catch = $myException 
+    $Return.Error_Count++ 
+}
+# END REGION
+
+# REGION Resize VSS to free disk space
+try {
+    # Format drive letter for VSS command
+    $myDrive = $Drive + ':'
+    # Resize ShadowStorage to 400mb to delete ShadowCopies
+    $Return.VSS_Resize = . vssadmin.exe resize shadowstorage /on=$myDrive /for=$myDrive /maxsize=400mb
+}
+catch {
+    $myException = $_.Exception | Format-List | Out-String
+    $Return.VSS_Resized_Catch = $myException 
+    $Return.Error_Count++ 
+}
+# END REGION
+
+# REGION Remove backup scheduled tasks
+try {
+    # Try to delete the scheduled tasks and capture the output, no errors unless it causes a catch
+    # Capture output as a string with stderr redirected to stdout then add to $Return so it looks prettier
+    [string]$BackupTask = . schtasks /Delete /TN "Microsoft\Windows\WindowsBackup\AutomaticBackup" /f 2>&1
+    [string]$MonitorTask = . schtasks /Delete /TN "Microsoft\Windows\WindowsBackup\Windows Backup Monitor" /f 2>&1
+    $Return.Backup_Task = $BackupTask
+    $Return.Monitor_Task = $MonitorTask
+}
+catch {
+    $myException = $_.Exception | Format-List | Out-String
+    $Return.Task_Removal_Catch = $myException 
     $Return.Error_Count++ 
 }
 # END REGION
