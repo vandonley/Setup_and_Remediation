13020bc040b55d0abc993dfd25cc8b2506622327
vandonley <vandonley@gmail.com>
2018-01-29 11:31:31 -0800

    Remove Windows 7  Backup

---
+++ vitRemove-WindowsBackup.ps1
@@ -0,0 +1,114 @@
+<#
+.Synopsis
+   Remove Windows 7 Backup folders and tasks
+.DESCRIPTION
+   The script is to be uploaded to your dashboard account as a user script.
+   It can run both as a script check and as a scheduled task. Expects to be run
+   with vitCheck-RMMDefaults.ps1
+.EXAMPLE
+   vitRemove-WindowsBackup
+.EXAMPLE
+   vitRemove-WindowsBackup -Drive 'Z'
+.OUTPUTS
+   Error file if needed and removes files
+.EMAIL
+   vdonley@visionms.net
+.VERSION
+   1.0
+#>
+<#
+ Others optional but must accept -logfile from MaxRM.
+#>  
+param (	
+    # Drive to cleanup, defaults to 'Z'
+    [Parameter()]
+    [string]
+    $Drive = 'Z',
+
+	# Make sure -logfile is NOT positional
+	[Parameter(Mandatory=$false)]
+	[string]$logfile
+)
+
+# Set error and warning preferences
+$ErrorActionPreference = "Continue"
+$WarningPreference = "Continue"
+
+# Force output to keep RMM from timing out
+Write-Host ' '
+
+# Create hashtable for output. Make it stay in order and start
+# an error counter to create an alert if needed.
+$Return = @{}
+$Return.Error_Count = 0
+
+# REGION Reporting setup
+try {
+    # Information about the script for reporting.
+    $ErrorFileName = "vitRemove-WindowsBackup.txt"
+    # File name for ScriptRunnter
+    $Return.RMM_Script_Name = $MyInvocation.MyCommand.Name
+    # Check to see if the RMM Error Folder exists. Put the Error file in %TEMP% if it doesn't.
+    $myErrorPath = $env:RMMErrorFolder
+    if ($myErrorPath) {
+        $Return.Error_File = $env:RMMErrorFolder + "\" + $ErrorFileName
+    }
+    else {
+        $Return.Error_File = $env:TEMP + "\" + $ErrorFileName
+    }
+}
+catch {
+    $myException = $_.Exception | Format-List | Out-String
+    $Return.File_Information_Catch = $myException 
+    $Return.Error_Count++ 
+}
+# END REGION
+
+# REGION Remove backup folders
+try {
+    # Folders to look for
+    $FileBackups = $Drive + ':\' + $Env:COMPUTERNAME
+    $ImageBackups = $Drive + ':\WindowsImageBackup'
+    # Check to see if the folders exist, then delete
+    if (Test-Path -Path $FileBackups) {
+        $Return.Backup_Files = Remove-Item
+    }      
+}
+catch {
+    $myException = $_.Exception | Format-List | Out-String
+    $Return.Drive_Change_Catch = $myException 
+    $Return.Error_Count++ 
+}
+# END REGION
+
+# REGION Output results and create an alert if needed
+if ($Return.Error_Count -eq 0) {
+    Write-Output @"
+    
+Script Success!
+Troubleshooting info below
+_______________________________
+   
+"@
+    $Return | Format-List | Out-String
+    if (Test-Path $Return.Error_File) {
+        Remove-Item $Return.Error_File
+    }
+    Exit 0
+    }
+else {
+    Write-Output @"
+    
+Script Failure!
+Troubleshooting info below
+_______________________________
+
+"@
+    $Return | Format-List | Out-String
+    Add-Content -Path $Return.Error_File -Value "`n----------------------`n "
+	Add-Content -Path $Return.Error_File -Value (get-date) -passthru
+	Add-Content -Path $Return.Error_File -Value "`n "
+	Add-Content -Path $Return.Error_File -Value ( $Return | Format-List | Out-String )
+    Exit 1001
+}
+# END REGION
