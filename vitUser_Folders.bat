178e5fbefb2ad379961ba67800a030a5fd00ae72
vandonley <vandonley@gmail.com>
2018-07-19 13:28:45 -0700

    Check for group and fixed ICACLS command

--- vitUser_Folders.bat
+++ vitUser_Folders.bat
@@ -7,10 +7,22 @@
 :: 03/21/2018
 ::
 :: Set user folder location
-SET myFolder="\\<SERVER>\Users\%USERNAME%"
+SET myFolder="\\<SERVER>\<Folder>\%USERNAME%"
+:: AD group to check membership for creating a folder
+SET myGroup="<AD Group Name>"
+::
+:: Check to see if the user is a member of the gorup
+FOR /f %%f in ('"NET user %USERNAME% /domain | FINDSTR /i %MyGroup%"') do SET /a i=%i%+1
+:: Check for folder if in group or exit
+IF %i% gtr 0 (GOTO :FOLDERCHECK)
+GOTO :END
+::
+:FOLDERCHECK
 :: Create folder and set permissions if it doesn't exist
 IF NOT EXIST %myFolder% (
     MD %myFolder%
-    ICACLS %myFolder% /grant %USERNAME%:M /t
+    ICACLS %myFolder% /grant "%USERDOMAIN%\%USERNAME%:(OI)(CI)M"
 )
+::
+:END
 EXIT
